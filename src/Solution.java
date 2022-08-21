efbca2f676e0291023ec6783798aa220827f2550
Pruthvi Kolhe <65055563+pruthvikx@users.noreply.github.com>
2022-08-21 17:17:08 +0530

    Get largest and smallest lexological string

---
+++ src/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+
+        String currString = s.substring(0,k);
+
+        String smallest = currString;
+        String largest = currString;
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        for (int i = 1;i <= (s.length() - k);i++){
+            currString = s.substring(i,(i + k));
+            if (currString.compareTo(smallest) < 0){
+                smallest = currString;
+            }
+            if (currString.compareTo(largest) > 0){
+                largest = currString;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
